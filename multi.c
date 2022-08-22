ce0fad5b4f8391f72318499e15d48941b114ab8b
Ndheeraj03 <raghudheeraj.03@gmail.com>
2022-08-22 19:08:42 +0530

    coco cola

---
+++ multi.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	printf("\n enter two numbers for multiplication");
+	scanf("%d %d",&a,&b);
+	c=a*b;
+	printf("\n the multiplication of two numbers is : %d",c);
+	return 0;
+}
