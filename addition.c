de97c24579060d8bf85a6ee3822dac893ee1b90e
Ndheeraj03 <raghudheeraj.03@gmail.com>
2022-08-22 18:46:56 +0530

    this is parctice

---
+++ addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	printf("\n enter two numbers for addition :");
+	scanf("%d %d",&a,&b);
+	c =a+b;
+	printf("\n the addition of two number is :%d ",c);
+	return 0;
+
+}
---
+++ hello.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+	printf("Hello anna namasthe :\n");
+	printf("jai balayya\n");
+	return 0;
+}
